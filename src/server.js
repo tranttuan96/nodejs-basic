8bd6d61f2655caeb644d22df4bf3bbb73c2c2318
Tran Thanh Tuan <tranttuan96@gmail.com>
2022-04-21 18:24:06 +0700

    about router and MVC

--- src/server.js
+++ src/server.js
@@ -1,15 +1,14 @@
 import express from 'express';
 import configViewEngine from './configs/viewEngine'
+import initWebRoute from './route/web'
+
 require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 8080;
 
 configViewEngine(app)
-
-app.get('/', (req, res) => {
-  res.render('index.ejs')
-})
+initWebRoute(app)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
