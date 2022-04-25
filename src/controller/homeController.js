d2c82dde96cebb7b9ac5d2a48d91c9ded7a36ad7
Tran Thanh Tuan <tranttuan96@gmail.com>
2022-04-25 21:11:23 +0700

    complete CRUD

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -17,8 +17,29 @@ let createNewUser = async (req, res) => {
     return res.redirect('/')
 }
 
+let deleteUser = async (req, res) => {
+    let {userId} = req.body;
+    await pool.execute('DELETE FROM users WHERE id = ?',[userId]);
+    return res.redirect('/')
+}
+
+let editUser = async (req, res) => {
+    let {userId} = req.params;
+    const [user] = await pool.execute(`SELECT * FROM users WHERE id = ?`,[userId]);
+    return res.render('update.ejs', { dataUser: user[0] });
+}
+
+let updateUser = async (req, res) => {
+    let {id, firstName, lastName, email, address} = req.body;
+    await pool.execute(`UPDATE users SET firstName = ?, lastName=?, email=?, address=? WHERE id=?`,[firstName, lastName, email, address, id]);
+    return res.redirect('/')
+}
+
 module.exports = {
     getHomepage,
     getDetailPage,
-    createNewUser
+    createNewUser,
+    deleteUser,
+    editUser,
+    updateUser
 };
